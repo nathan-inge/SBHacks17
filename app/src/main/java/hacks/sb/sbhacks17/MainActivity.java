73ce3e0cc880136b864b22b2729709dea087404c
Nathan <ninge@umail.ucsb.edu>
2017-01-20 23:58:34 -0800

    Base maps added

--- app/src/main/java/hacks/sb/sbhacks17/MainActivity.java
+++ app/src/main/java/hacks/sb/sbhacks17/MainActivity.java
@@ -3,16 +3,34 @@ package hacks.sb.sbhacks17;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+
 public class MainActivity extends AppCompatActivity {
+    private MapView mMapView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is a commit to see if this works
-        //This is a change I am making in Android Studio
-        //Nathans change
-        //I really hope rick can see this
-        //i hope you can see this nate
+
+        mMapView = (MapView) findViewById(R.id.mapView);
+        ArcGISMap map = new ArcGISMap(Basemap.Type.IMAGERY_WITH_LABELS, 34.056295, -117.195800, 16);
+        mMapView.setMap(map);
+    }
+
+    @Override
+    protected void onPause(){
+        mMapView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.resume();
     }
 }
